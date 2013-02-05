59b9eff02740ca6ee8885a335e480e1434d7f7d8
Robb Hedrick <robbhedrick@gmail.com>
2013-02-05 15:30:41 -0500

    added mongolab address and remove script from worker and added to web.

--- web.js
+++ web.js
@@ -6,6 +6,19 @@ app.get('/', function(request, response) {
   response.send('Hello World!');
 });
 
+var mongo = require('mongodb');
+
+var mongoUri = process.env.MONGOLAB_URI || 
+  process.env.MONGOHQ_URL || 
+  'mongodb://dbuser:user4now@ds031877.mongolab.com:31877/robbhedrick-test'; 
+
+mongo.Db.connect(mongoUri, function (err, db) {
+  db.collection('users', function(er, collection) {
+    collection.insert({'name': 'dick seta'}, {safe: true}, function(er,rs) {
+    });
+  });
+});
+
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
